e8cf391e81d5a3356998473fe66571c7ec24dc93
Himanshu Parwani <Himanshu@himanshu.parwani>
2021-01-10 19:58:58 +0530

    written test cases

--- test/TodoTest.php
+++ test/TodoTest.php
@@ -4,8 +4,10 @@ class TodoTest extends PHPUnit_Framework_TestCase
 {
     public function testLoginUser() {
         include __DIR__ . '/../app/TestUser.php';
+        $name = "Karan";
+        $email = "karanparwani.parwani102@gmail.com";
         $expectedStatus = 201;
-        $this->assertEquals(201, checkLoginUser("Karan", "karanparwani.parwani102@gmail.com"));
+        $this->assertEquals(201, checkLoginUser($name, $email));
     } 
 
     public function testLoadingOwnProfile() {
@@ -14,4 +16,39 @@ class TodoTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(200, checkProfile($uid));
     }
 
+    public function testLoadOtherProfile() {
+        $uid = 'fasfhsafsfykayrexafghfakfbak';
+        $expectedStatus = 200;
+        $this->assertEquals(200, checkProfile($uid));
+    }
+
+    public function testLoadFriends() {
+        $uid = 'fasfhsafsfykayrexafghfakfbak';
+        $expectedStatus = 200;
+        $this->assertEquals(200, checkProfile($uid));
+    }
+
+    public function testLoadingTimelinePost() {
+        $uid = 'fasfhsafsfykayrexafghfakfbak';
+        $expectedStatus = 200;
+        $this->assertEquals(200, checkProfile($uid));
+    }
+
+    public function testLikes() {
+        $uid = 'fasfhsafsfykayrexafghfakfbak';
+        $expectedStatus = 200;
+        $this->assertEquals(200, checkProfile($uid));
+    }
+
+    public function testComments() {
+        $uid = 'fasfhsafsfykayrexafghfakfbak';
+        $expectedStatus = 200;
+        $this->assertEquals(200, checkProfile($uid));
+    }
+
+    public function testUploadPost() {
+        $uid = 'fasfhsafsfykayrexafghfakfbak';
+        $expectedStatus = 200;
+        $this->assertEquals(200, checkProfile($uid));
+    }
 }
